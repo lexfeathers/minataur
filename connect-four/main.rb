ad61b572c5ea0d9e2d18e757a8d3d80240beb24a
Lex <38232476+lexfeathers@users.noreply.github.com>
2025-02-17 11:27:16 -0500

    Using _ as variable names

--- connect-four/main.rb
+++ connect-four/main.rb
@@ -1,13 +1,14 @@
+# Snake case for variables, camelcase for classes
 # Define our board
-board = ((1..6).map{ |column| (1..7).map{ |row| 0 }.join('|') })
+board = ((1..6).map{ |_| (1..7).map{ |_| 0 }.join('|') })
 # The same thing, but indented:
-# board = ((1..6).map do |column| 
-#   (1..7).map do |row|
+# board = ((1..6).map do |_| 
+#   (1..7).map do |_|
 #     0
 #   end.join('|') 
 # end)
 
-# Snake case for variables, camelcase for classes
+# def is for writing functions, which are called methods here
 def print_board(current_board)
   # Print the current state of the board
   puts current_board
