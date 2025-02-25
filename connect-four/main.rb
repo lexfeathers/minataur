0f1f00ff79bf50bab70898a43c8e954e0f81a6f0
Lex <38232476+lexfeathers@users.noreply.github.com>
2025-02-24 22:08:06 -0500

    Got the board updating with the player value at the selected column

--- connect-four/main.rb
+++ connect-four/main.rb
@@ -11,21 +11,30 @@ board = ((1..6).map{ |_| (1..7).map{ |_| 0 } })
 # def is for writing functions, which are called methods in ruby
 def print_board(current_board)
   # Print the current state of the board
+  puts ""
   puts current_board.map(&:join)
 end
 
 # update a chosen index/column of the board array with the player number
-def token_drop(current_board, player, column)
+def token_drop(current_board, player, input_column)
   row = 0
-  for i in current_board
-    if current_board[row] = 0
+
+  # Trying to iterate through the rows, checking if the current value is zero
+  for i in current_board do
+    if current_board[row][input_column] = 0
+      current_board[row][input_column] = player
+    else
       row += 1
-      # puts row
-    end
+    end 
   end
-  current_board[row][column] = player
   print_board(current_board)
 end
 
+player = 1
 # drop a token
-token_drop(board, 1, 1)
+token_drop(board, player, 3)
+# token_drop(board, player, 3)
+
+# board.each_with_index{ |row, idx|
+#   puts "The current row is #{row} at index #{idx}"
+# }
